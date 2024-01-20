181046f0a2d625418f8df88d0073c333db0b5265
marat <maratxat@ya.ru>
2024-01-20 20:32:45 +0300

    Add app_path.dart with base path configuration

---
+++ lib/navigation/app_path.dart
@@ -0,0 +1,9 @@
+abstract class AppPath {}
+
+abstract class MainPath extends AppPath {}
+
+class LaunchPath extends MainPath {}
+
+class HomePath extends LaunchPath {}
+
+class UndefinedPath extends LaunchPath {}
